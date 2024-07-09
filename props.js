7708c3eb6867805451e6f9bc2c6ceacfa7656a90
pylori-bronzes <pylori.bronzes.0f@icloud.com>
2024-07-10 01:54:00 +0700

    Fixed bug in data migration script

---
+++ props.js
@@ -0,0 +1,12 @@
+function* fibonacci() {
+  let [prev, curr] = [0, 1];
+  while (true) {
+    yield curr;
+    [prev, curr] = [curr, prev + curr];
+  }
+}
+
+const fib = fibonacci();
+for (let i = 0; i < 10; i++) {
+  console.log(fib.next().value);
+}
